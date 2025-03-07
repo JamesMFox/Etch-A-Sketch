913fe54082fcb3a5829f44fe33aaf335b526b004
JamesMFox <31540421+JamesMFox@users.noreply.github.com>
2025-03-06 18:00:39 -0600

    added comments to code

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
+//////////////////////////////////////
+/// set variables and DOM elements ///
+//////////////////////////////////////
 const gridContainer = document.createElement("div");
 gridContainer.setAttribute("class", "flex-container");
-
 const resetBtn = document.getElementById("reset-btn");
 const newGrid = document.getElementById("change-grid");
 var opacityValue = 0;
 
+// initialize grid with default value (16)
 changeGrid();
 
-
+// paint the element when mouseover
 gridContainer.addEventListener("mouseover", (e) => {
-  
   e.target.classList.add("painted");
   paintCell(e.target);
 });
 
+// reset the board
 resetBtn.addEventListener("click", () => {
   reset();
 });
 
+// change the grid size to user input 1-100
 newGrid.addEventListener("click", () => {
   changeGrid(getNewSize());
 })
 
-function paintCell(cell){
+
+ /////////////////
+ /// FUNCTIONS ///
+ /////////////////
+
+ //sets background randomly and opacity
+ function paintCell(cell){
   let r = Math.floor(Math.random() * 255);
   let g = Math.floor(Math.random() * 255);
   let b = Math.floor(Math.random() * 255);
@@ -30,9 +40,9 @@ function paintCell(cell){
   if (opacityValue < 10 ) {
     opacityValue += 1;
   };
-  
 }
 
+// prompts for new grid size 1 - 100
 function getNewSize() {
   let entry = 0;
   do {
@@ -42,6 +52,7 @@ function getNewSize() {
   return entry;
 }
 
+// draws the grid
 function changeGrid(newSize = 16) {
   gridContainer.innerHTML = "";
   for ( let i = 0; i < newSize; i++){
@@ -54,18 +65,20 @@ function changeGrid(newSize = 16) {
   document.body.appendChild(gridContainer);
 };
 
+// creates new row div
 function createRowContainer() {
   const rowContainer = document.createElement("div");
   rowContainer.setAttribute("class", "flex-row");
   return rowContainer;
 };
-
+// creates new cell div
 function createElementDiv() {
   const elementDiv = document.createElement("div");
   elementDiv.setAttribute("class", "flex-item");
   return elementDiv;
 };
 
+//resets background and classes to default
 function reset() {
   const elementArr = Array.from(document.getElementsByClassName("painted"));
   for ( let i = 0; i < elementArr.length; i++) {
